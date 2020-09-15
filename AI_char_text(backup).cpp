591dcbe48c0eb3cf52eb919fed5308f91d1ba6c6
Kaori <super_so_nic1@hotmail.com>
2020-09-15 18:07:57 -0500

    Correcting the code to allow the use of Configure option in the Mod Loader. Deleting DC variables.

--- AI_char_text(backup).cpp
+++ AI_char_text(backup).cpp
@@ -6,10 +6,14 @@
 #define ReplacePVM(a, b) helperFunctions.ReplaceFile("system\\" a ".PVM", "system\\" b ".PVM");
 
 //Options
+enum KindofTextureDC { OriginalDC, UekawaDC };
 enum KindofTextureDX { LikeDream, OriginalDX, DreamcastT, UekawaDX };
+enum DCSuperS { OriginalDCSS, UpgradedDC };
 enum DXSuperS { LikeDreamSS, OriginalDXSS, DreamcastTSS, UpgradedDX };
 
+static int KindofTextDC = OriginalDC;
 static int KindofTextDX = LikeDream;
+static int DCSS = UpgradedDC;
 static int DXSS = UpgradedDX;
 
 extern "C"
@@ -18,19 +22,44 @@ extern "C"
 	{
 		//Functions
 		const IniFile* config = new IniFile(std::string(path) + "\\config.ini");
+		std::string KindofTextDC_String = "OriginalDC";
+		KindofTextDC_String = config->getString("Textures", "DreamcastChars", "OriginalDC");
 		std::string KindofTextDX_String = "LikeDream";
 		KindofTextDX_String = config->getString("Textures", "DXChars", "LikeDream");
+		std::string DCSS_String = "UpgradedDC";
+		DCSS_String = config->getString("Textures", "DreamcastSS", "OriginalDCSS");
 		std::string DXSS_String = "UpgradedDX";
 		DXSS_String = config->getString("Textures", "DXSS", "OriginalDXSS");
+		if (KindofTextDC_String == "OriginalDC") KindofTextDC = OriginalDC;
+		if (KindofTextDC_String == "UekawaDC") KindofTextDC = UekawaDC;
 		if (KindofTextDX_String == "LikeDream") KindofTextDX = LikeDream;
 		if (KindofTextDX_String == "DreamcastT") KindofTextDX = DreamcastT;
 		if (KindofTextDX_String == "OriginalDX") KindofTextDX = OriginalDX;
 		if (KindofTextDX_String == "UekawaDX") KindofTextDX = UekawaDX;
-		if (DXSS_String == "LikeDreamSS") DXSS = LikeDreamSS;
-		if (DXSS_String == "DreamcastTSS") DXSS = DreamcastTSS;
-		if (DXSS_String == "OriginalDXSS") DXSS = OriginalDXSS;
-		if (DXSS_String == "UpgradedDX") DXSS = UpgradedDX;
+		if (DCSS_String == "OriginalDCSS") DCSS = OriginalDCSS;
+		if (DCSS_String == "UpgradedDC") DCSS = UpgradedDC;
+		if (DXSS_String == "LikeDreamSS") DCSS = LikeDreamSS;
+		if (DXSS_String == "DreamcastTSS") DCSS = DreamcastTSS;
+		if (DXSS_String == "OriginalDXSS") DCSS = OriginalDXSS;
+		if (DXSS_String == "UpgradedDX") DCSS = UpgradedDX;
 		
+
+		if (KindofTextDC == UekawaDC)
+		{
+			ReplacePVM("amy_dc", "AMY_DCu");
+			ReplacePVM("big_dc", "BIG_DCu");
+			ReplacePVM("ev_tr1_with_sonic_dc", "ev_tr1_with_sonic_dcu");
+			ReplacePVM("ev_tr2before_with_sonic_dc", "ev_tr2before_with_sonic_dcu");
+			ReplacePVM("ev_tr2change_with_sonic_dc", "ev_tr2change_with_sonic_dcu");
+			ReplacePVM("knuckles_dc", "knuckles_dcu");
+			ReplacePVM("m_head_1_dc", "m_head_1_dcu");
+			ReplacePVM("m_tr_p_dc", "m_tr_p_dcu");
+			ReplacePVM("Miles_dc", "Miles_DCu");
+			ReplacePVM("shooting1_dc", "shooting1_dcu");
+			ReplacePVM("shooting2_dc", "shooting2_dcu");
+			ReplacePVM("SONIC_dc", "SONIC_DCu");
+		}
+
 		if (KindofTextDX == LikeDream)
 		{
 			ReplacePVM("amy", "amy_ld");
@@ -65,6 +94,7 @@ extern "C"
 			ReplacePVM("SONIC", "SONIC_dxd");
 		}
 
+
 		if (KindofTextDX == UekawaDX)
 		{
 			ReplacePVM("amy", "AMY_dxu");
@@ -80,6 +110,11 @@ extern "C"
 			ReplacePVM("shooting1", "shooting1_dxu");
 			ReplacePVM("shooting2", "shooting2_dxu");
 			ReplacePVM("SONIC", "SONIC_dxu");
+		}
+
+		if (DCSS == UpgradedDC)
+		{
+			ReplacePVM("supersonic_dc", "supersonic_dcn")
 		}
 
 		if (DXSS == UpgradedDX)
